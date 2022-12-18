c59c387edba6e5ca9cf54363d5ab8d8630987813
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-12-18 14:16:58 +0900

    add: 石炭、石英、硫黄関連のアイテムと基本レシピを追加

--- satisfactory_calc/define.py
+++ satisfactory_calc/define.py
@@ -2,6 +2,8 @@ import nlib3
 
 
 class Item(nlib3.StrEnum):
+    # Coal
+    coal = "Coal"
     # Iron
     iron_ore = "Iron Ore"
     iron_ingot = "Iron Ingot"
@@ -11,13 +13,22 @@ class Item(nlib3.StrEnum):
     # Copper
     copper_ore = "Copper Ore"
     copper_ingot = "Copper Ingot"
+    # Raw Quartz
+    raw_quartz = "Raw Quartz"
+    quartz_crystal = "Quartz Crystal"
+    silica = "Silica"
     # Caterium
     caterium_ore = "Caterium Ore"
     caterium_ingot = "Caterium Ingot"
     quickwire = "Quickwire"
+    # Sulfur
+    sulfur = "Sulfur"
+    black_powder = "Black Powder"
+    compacted_coal = "Compacted Coal"
     # Liquid
     water = "Water"
     crude_oil = "Crude Oil"
+    sulfuric_acid = "Sulfuric Acid"
     # Nuclear power
     uranium = "Uranium"
     encased_uranium_cell = "Encased Uranium Cell"
@@ -33,4 +44,5 @@ class Building(nlib3.StrEnum):
     Manufacturer = "Manufacturer"           # 製造機
     water_extractor = "Water Extractor"     # 揚水ポンプ
     oil_extractor = "Oil Extractor"         # 原油抽出機
+    refinery = "Refinery"                   # 精製施設
     othre = "othre"
--- satisfactory_calc/recipe.py
+++ satisfactory_calc/recipe.py
@@ -27,13 +27,23 @@ RECIPE: Final[dict[str | Item, Recipe]] = {
                                                                                                                                                                  # Copper
     Item.copper_ore: Recipe(RecipeIO(), RecipeIO(Item.copper_ore, 780), Building.miner_mk3),
     Item.copper_ingot: Recipe(RecipeIO(Item.copper_ore, 30), RecipeIO(Item.copper_ingot, 30), Building.constructor),
+                                                                                                                                                                 # Raw Quartz
+    Item.raw_quartz: Recipe(RecipeIO(), RecipeIO(Item.raw_quartz, 780), Building.miner_mk3),
+    Item.quartz_crystal: Recipe(RecipeIO(Item.raw_quartz, 37.5), RecipeIO(Item.quartz_crystal, 22.5), Building.constructor),
+    Item.silica: Recipe(RecipeIO(Item.raw_quartz, 22.5), RecipeIO(Item.silica, 37.5), Building.constructor),
                                                                                                                                                                  # Caterium
     Item.caterium_ore: Recipe(RecipeIO(), RecipeIO(Item.caterium_ore, 780), Building.miner_mk3),
     Item.caterium_ingot: Recipe(RecipeIO(Item.caterium_ore, 45), RecipeIO(Item.caterium_ingot, 15), Building.smelter),
     alternate(Item.caterium_ingot): Recipe(RecipeIO(Item.caterium_ore, 24).add_item(Item.water, 24), RecipeIO(Item.caterium_ingot, 12), Building.smelter),
     Item.quickwire: Recipe(RecipeIO(Item.caterium_ingot, 12), RecipeIO(Item.quickwire, 60), Building.constructor),
     alternate(Item.quickwire): Recipe(RecipeIO(Item.caterium_ingot, 7.5).add_item(Item.copper_ingot, 37.5), RecipeIO(Item.quickwire, 90), Building.constructor),
+                                                                                                                                                                 # Sulfur
+    Item.sulfur: Recipe(RecipeIO(), RecipeIO(Item.sulfur, 780), Building.miner_mk3),
+    Item.black_powder: Recipe(RecipeIO(Item.coal, 15).add_item(Item.sulfur, 15), RecipeIO(Item.black_powder, 30), Building.assembler),
+    Item.compacted_coal: Recipe(RecipeIO(Item.coal, 25).add_item(Item.sulfur, 25), RecipeIO(Item.compacted_coal, 25), Building.assembler),
                                                                                                                                                                  # Liquid
     Item.water: Recipe(RecipeIO(), RecipeIO(Item.water, 120), Building.water_extractor),
     Item.crude_oil: Recipe(RecipeIO(), RecipeIO(Item.crude_oil, 120), Building.oil_extractor),
+    Item.sulfuric_acid: Recipe(RecipeIO(Item.sulfur, 50).add_item(Item.water, 50), RecipeIO(Item.sulfuric_acid, 50), Building.refinery),
+                                                                                                                                                                 # Nuclear power
 }
