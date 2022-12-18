46f21310749d35a533e93380fbe3a2eb690c0cd5
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-12-18 15:10:29 +0900

    fix: パッケージ名が反映されていない不具合を修正

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import sf_calc
-import recipe
-from define import Item
+import satisfactory_calc as sf_calc
+from satisfactory_calc import RECIPE, Ingredients, Purity
 
-root_node_screw = sf_calc.RecipeNode(recipe.RECIPE[Item.screw])
-node_iron_rod = sf_calc.RecipeNode(recipe.RECIPE[Item.iron_rod], parent=root_node_screw)
-node_iron_ingot = sf_calc.RecipeNode(recipe.RECIPE[Item.iron_ingot], parent=node_iron_rod)
-node_iron_ore = sf_calc.RecipeNode(recipe.RECIPE[Item.iron_ore], parent=node_iron_ingot)
+root_node_screw = sf_calc.RecipeNode(RECIPE[Ingredients.screw])
+node_iron_rod = sf_calc.RecipeNode(RECIPE[Ingredients.iron_rod], parent=root_node_screw)
+node_iron_ingot = sf_calc.RecipeNode(RECIPE[Ingredients.iron_ingot], parent=node_iron_rod)
+node_iron_ore = sf_calc.RecipeNode(RECIPE[Ingredients.iron_ore].with_purity(Purity.pure).with_clock_speed(2.5), parent=node_iron_ingot)
 
 print(root_node_screw)
 print(root_node_screw.get_recipe_tree_str())
