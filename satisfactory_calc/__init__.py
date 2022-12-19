ed9b3d1a40c930dc9e0571b328e7b62e5e451e12
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-12-19 18:22:43 +0900

    v1.2.0

--- satisfactory_calc/__init__.py
+++ satisfactory_calc/__init__.py
@@ -3,7 +3,7 @@ from .recipe import *
 from .sf_calc import *
 
 __copyright__: Final[str] = "Copyright (C) 2022 Nicoyou"
-__version__: Final[str] = "1.1.0"
+__version__: Final[str] = "1.2.0"
 __license__: Final[str] = "MIT"
 __author__: Final[str] = "nicoyou"
 __author_email__: Final[str] = "nicoyou.info@gmail.com"
